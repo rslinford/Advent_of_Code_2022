bca1bec25fc1c52fb34cec0342d1b9468e438272
Scott <scott@linfords.com>
2022-12-01 04:00:44 -0800

    Initial commit

---
+++ Day_01.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+def read_puzzle_input(filename):
+    with open(filename, 'r') as f:
+        data = f.read()
+    return data
+
+    return depth
+
+
+def part_one(filename):
+    data = read_puzzle_input(filename)
+
+def part_two(filename):
+    data = read_puzzle_input(filename)
+
+
+part_one('Day_01_input.txt')
+
+class Test(unittest.TestCase):
+    def test_xxx(self):
+        pass
