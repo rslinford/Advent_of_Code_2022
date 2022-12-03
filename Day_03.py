f11ca4f6e1dcbd7e950fcd9f62905d6c99568577
Scott <scott@linfords.com>
2022-12-02 23:00:15 -0800

    Day 3 part 1 passes.

--- Day_03.py
+++ Day_03.py
@@ -23,15 +23,29 @@ def find_common_letter(s1: str, s2: str) -> str:
     raise ValueError(f'No duplicate found in {s1} and {s2}')
 
 
+def lookup_type_rank(type: str) -> int:
+    if type >= 'a' and type <= 'z':
+        return ord(type) - ord('a') + 1
+    if type >= 'A' and type <= 'Z':
+        return ord(type) - ord('A') + 27
+
+
+def tally_ranks(rucksacks: (str, str)) -> int:
+    tally = 0
+    for rucksack in rucksacks:
+        c = find_common_letter(rucksack[0], rucksack[1])
+        tally += lookup_type_rank(c)
+    return tally
+
+
 def part_one(filename):
     data = read_puzzle_input(filename)
     rucksacks = parse_data(data)
-    print(rucksacks)
-    return 0
+    tally = tally_ranks(rucksacks)
+    return tally
 
 
-# print(f'Answer part one: {part_one("Day_03_short_input.txt")}')
-
+print(f'Answer part one: {part_one("Day_03_input.txt")}')
 
 class Test(unittest.TestCase):
     def test_parse_data(self):
@@ -51,3 +65,14 @@ class Test(unittest.TestCase):
         self.assertEqual(find_common_letter(rucksacks[3][0], rucksacks[3][1]), 'v')
         self.assertEqual(find_common_letter(rucksacks[4][0], rucksacks[4][1]), 't')
         self.assertEqual(find_common_letter(rucksacks[5][0], rucksacks[5][1]), 's')
+
+    def test_lookup_type_rank(self):
+        self.assertEqual(lookup_type_rank('a'), 1)
+        self.assertEqual(lookup_type_rank('z'), 26)
+        self.assertEqual(lookup_type_rank('A'), 27)
+        self.assertEqual(lookup_type_rank('Z'), 52)
+
+    def test_tally_ranks(self):
+        data = read_puzzle_input('Day_03_short_input.txt')
+        rucksacks = parse_data(data)
+        self.assertEqual(tally_ranks(rucksacks), 157)
