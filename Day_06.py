d25a217d5a6ae88ce3449c3f72b976baf4de7948
Scott <scott@linfords.com>
2022-12-05 22:45:21 -0800

    Day 6 part one passes, complete, tested

--- Day_06.py
+++ Day_06.py
@@ -1,22 +1,23 @@
-import re
 import unittest
 
 
 def read_puzzle_input(filename):
-    data = []
     with open(filename, 'r') as f:
-        for line in f:
-            if line[-1] == '\n':
-                data.append(line[:-1])
-            else:
-                data.append(line)
-
+        data = f.read()
     return data
 
 
 def part_one(filename):
     data = read_puzzle_input(filename)
+    for i,c in enumerate(data):
+        four = set()
+        for j in range(i,i+4):
+            four.add(data[j])
+        if len(four) == 4:
+            return i + 4
+        # print(i, c, four)
     return -1
+
 
 def part_two(filename):
     data = read_puzzle_input(filename)
@@ -25,15 +26,15 @@ def part_two(filename):
 
 filename = 'Day_06_input.txt'
 short_filename = 'Day_06_short_input.txt'
-print(f'Answer part one: {part_one(short_filename)}')
+print(f'Answer part one: {part_one(filename)}')
 print(f'Answer part two: {part_two(short_filename)}')
 
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(-1, part_one(short_filename))
-        self.assertEqual(-1, part_one(filename))
+        self.assertEqual(5, part_one(short_filename))
+        self.assertEqual(1356, part_one(filename))
 
     def test_part_two(self):
         self.assertEqual(-1, part_two(short_filename))
-        self.assertEqual(-1, part_two(filename))
+        self.assertEqual(-1, part_two(short_filename))
