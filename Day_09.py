86474031370722da599cc388127313db1f2cec3c
Scott <scott@linfords.com>
2022-12-09 08:42:00 -0800

    Day 9 movement code complete, not tested yet

--- Day_09.py
+++ Day_09.py
@@ -3,6 +3,93 @@ import unittest
 import numpy as np
 
 
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.history = set()
+        self.add_current_to_history()
+
+    def add_current_to_history(self):
+        self.history.add((self.x, self.y))
+
+    def move_right(self):
+        self.x += 1
+        self.add_current_to_history()
+
+    def move_left(self):
+        self.x -= 1
+        self.add_current_to_history()
+
+    def move_down(self):
+        self.y += 1
+        self.add_current_to_history()
+
+    def move_up(self):
+        self.y -= 1
+        self.add_current_to_history()
+
+class RopeSimulator():
+    def __init__(self):
+        self.start = Point(0,0)
+        self.head = Point(self.start.x, self.start.y)
+        self.tail = Point(self.start.x, self.start.y)
+
+    def move_head_right(self):
+        self.head.move_right()
+        self.consider_tail()
+
+    def move_head_left(self):
+        self.head.move_left()
+        self.consider_tail()
+
+    def move_head_up(self):
+        self.head.move_up()
+        self.consider_tail()
+
+    def move_head_down(self):
+        self.head.move_down()
+        self.consider_tail()
+
+    def head_and_tail_delta(self):
+        return self.head.x - self.tail.x, self.head.y - self.tail.y
+
+    def head_and_tail_touch(self):
+        delta_x, delta_y = self.head_and_tail_delta()
+        return abs(delta_x) < 2 and abs(delta_y) < 2
+
+    def consider_tail(self):
+        if self.head_and_tail_touch():
+            return
+        delta_x, delta_y = self.head_and_tail_delta()
+        # Non-diagonal
+        if delta_x == 0:
+            assert abs(delta_y) == 2
+            if delta_y > 0:
+                self.tail.move_down()
+            else:
+                self.tail.move_up()
+            return
+        elif delta_y == 0:
+            assert abs(delta_x) == 2
+            if delta_x < 0:
+                self.tail.move_left()
+            else:
+                self.tail.move_right()
+            return
+        # Diagonal
+        assert abs(delta_x) > 0
+        assert abs(delta_y) > 0
+        if delta_y > 0:
+            self.tail.move_down()
+        else:
+            self.tail.move_up()
+        if delta_x < 0:
+            self.tail.move_left()
+        else:
+            self.tail.move_right()
+        return
+
 def read_puzzle_input(filename):
     with open(filename, 'r') as f:
         data = f.read().strip().split('\n')
@@ -11,8 +98,14 @@ def read_puzzle_input(filename):
 
 def part_one(filename):
     data = read_puzzle_input(filename)
-    for line in data:
-        print(line)
+    rs = RopeSimulator()
+    rs.move_head_right()
+    touch = rs.head_and_tail_touch()
+    rs.move_head_down()
+    touch = rs.head_and_tail_touch()
+    rs.move_head_down()
+    touch = rs.head_and_tail_touch()
+    rs.consider_tail()
     return -1
 
 
