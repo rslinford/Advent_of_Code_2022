d5b900064b16c54a22673407e5f86ef4cec568d3
Scott <scott@linfords.com>
2022-12-12 11:50:46 -0800

    Day 12 part one in progress, colorized Hiker rendering

--- Day_12.py
+++ Day_12.py
@@ -1,7 +1,7 @@
 import unittest
 
 import numpy as np
-
+from colorama import Fore, Back, Style
 
 def read_puzzle_input(filename):
     with open(filename, 'r') as f:
@@ -53,8 +53,8 @@ class MapGrid:
                     return (x, y)
 
     def delta_altitude_between(self, x1, y1, x2, y2):
-        altitude1 = self.map.altitude_at(x1, y1)
-        altitude2 = self.map.altitude_at(x2, y2)
+        altitude1 = self.altitude_at(x1, y1)
+        altitude2 = self.altitude_at(x2, y2)
         delta_altitude = altitude2 - altitude1
         return delta_altitude
 
@@ -64,59 +64,83 @@ class Hiker:
         self.map = map
         # self.current_x, self.current_y = map.find_start()
         self.end_x, self.end_y = map.find_end()
-        self.step_tally = 0
-        self.bread_crumbs = set()
+        # self.step_tally = 0
+        # self.bread_crumbs = set()
+
+    def render_map(self, current_x, current_y, bread_crumbs):
+        rval = []
+        for y in range(self.map.grid.shape[0]):
+            if y > 0:
+                rval.append('\n')
+            for x in range(self.map.grid.shape[1]):
+                c = self.map.grid[y][x].decode()
+                if x == current_x and y == current_y:
+                    rval.append(Fore.RED)
+                    rval.append(c)
+                    rval.append(Style.RESET_ALL)
+                elif (x, y) in bread_crumbs:
+                    rval.append(Fore.LIGHTCYAN_EX)
+                    rval.append(c)
+                    rval.append(Style.RESET_ALL)
+                else:
+                    rval.append(c)
+        return ''.join(rval)
 
     def explore(self):
         x, y = self.map.find_start()
-        return self.explore_location(x, y)
+        return self.explore_location(x, y, -1, set())
 
-    def explore_location(self, x, y):
-        if (x, y) in self.bread_crumbs:
-            return self.step_tally
-        self.bread_crumbs.add((x, y))
-        self.step_tally += 1
+    def explore_location(self, x, y, step_tally, bread_crumbs):
+        if (x, y) in bread_crumbs:
+            return
+        bread_crumbs.add((x, y))
+        step_tally += 1
+        print(f'\nStep {step_tally}')
+        print(self.render_map(x, y, bread_crumbs))
         if x == self.end_x and y == self.end_y:
+            print(f'Step taken {step_tally}')
             # End Found. todo: collect possible solutions here
-            return self.step_tally
-        self.look_north_of(x, y)
-        self.look_south_of(x, y)
-        self.look_east_of(x, y)
-        self.look_west_of(x, y)
-        return self.step_tally
+            return
+        self.look_north_of(x, y, step_tally, bread_crumbs.copy())
+        self.look_south_of(x, y, step_tally, bread_crumbs.copy())
+        self.look_east_of(x, y, step_tally, bread_crumbs.copy())
+        self.look_west_of(x, y, step_tally, bread_crumbs.copy())
+        return
 
-    def look_north_of(self, x, y):
+    def look_north_of(self, x, y, step_tally, bread_crumbs):
         if y <= 0:
             return
         delta = self.map.delta_altitude_between(x, y, x, y - 1)
         if delta <= 1:
-            self.explore_location(x, y - 1)
+            self.explore_location(x, y - 1, step_tally, bread_crumbs)
 
-    def look_south_of(self, x, y):
+    def look_south_of(self, x, y, step_tally, bread_crumbs):
         if y >= self.map.grid.shape[0] - 1:
             return
         delta = self.map.delta_altitude_between(x, y, x, y + 1)
         if delta <= 1:
-            self.explore_location(x, y + 1)
+            self.explore_location(x, y + 1, step_tally, bread_crumbs)
 
-    def look_east_of(self, x, y):
+    def look_east_of(self, x, y, step_tally, bread_crumbs):
         if x >= self.map.grid.shape[1] - 1:
             return
         delta = self.map.delta_altitude_between(x, y, x + 1, y)
         if delta <= 1:
-            self.explore_location(x + 1, y)
+            self.explore_location(x + 1, y, step_tally, bread_crumbs)
 
-    def look_west_of(self, x, y):
+    def look_west_of(self, x, y, step_tally, bread_crumbs):
         if x <= 0:
             return
         delta = self.map.delta_altitude_between(x, y, x - 1, y)
         if delta <= 1:
-            self.explore_location(x - 1, y)
+            self.explore_location(x - 1, y, step_tally, bread_crumbs)
 
 
 def part_one(filename):
     data = read_puzzle_input(filename)
-    grid = MapGrid(data)
+    map = MapGrid(data)
+    hiker = Hiker(map)
+    hiker.explore()
     return -1
 
 
@@ -131,14 +155,14 @@ print(f'Answer part one: {part_one(short_filename)}')
 print(f'Answer part two: {part_two(short_filename)}')
 
 
-class Test(unittest.TestCase):
-    def test_part_one(self):
-        self.assertEqual(-1, part_one(short_filename))
-        self.assertEqual(-1, part_one(long_filename))
-
-    def test_part_two(self):
-        self.assertEqual(-1, part_two(short_filename))
-        self.assertEqual(-1, part_two(long_filename))
+# class Test(unittest.TestCase):
+#     def test_part_one(self):
+#         self.assertEqual(-1, part_one(short_filename))
+#         self.assertEqual(-1, part_one(long_filename))
+#
+#     def test_part_two(self):
+#         self.assertEqual(-1, part_two(short_filename))
+#         self.assertEqual(-1, part_two(long_filename))
 
 
 class TestMapGrid(unittest.TestCase):
