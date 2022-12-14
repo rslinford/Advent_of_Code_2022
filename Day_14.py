a02f322268871aef7ff76e4918cfbfaf5947ade6
Scott <scott@linfords.com>
2022-12-13 22:26:12 -0800

    Day 14 parsing done

--- Day_14.py
+++ Day_14.py
@@ -1,3 +1,6 @@
+import math
+
+import numpy as np
 import re
 import unittest
 
@@ -7,9 +10,68 @@ def read_puzzle_input(filename):
         data = f.read().strip().split('\n')
     return data
 
+class CaveMap:
+    grid: np.chararray
+
+    def __init__(self, min_x, max_x, min_y, max_y):
+        width = max_x - min_x + 1
+        height = max_y - min_y + 1
+        self.grid = np.chararray((height, width))
+        self.grid[:] = '.'
+        self.min_x = min_x
+        self.min_y = min_y
+
+    def __repr__(self):
+        rval = []
+        for y in range(self.grid.shape[0]):
+            if y > 0:
+                rval.append('\n')
+            for x in range(self.grid.shape[1]):
+                rval.append(self.grid[y][x].decode())
+        return ''.join(rval)
+
+
+def parse_points(data):
+    """Parse lines of data into rows of x,y points"""
+    rval = []
+    for line in data:
+        points = []
+        rval.append(points)
+        for p in line.split(' -> '):
+            result = re.search('(\d+),(\d+)', p)
+            x = int(result.group(1))
+            y = int(result.group(2))
+            points.append((x,y))
+
+    return rval
+
+
+def determine_xy_min_max(rows_of_points):
+    min_x = math.inf
+    min_y = math.inf
+    max_x = -math.inf
+    max_y = -math.inf
+    for row in rows_of_points:
+        for point in row:
+            x, y = point[0], point[1]
+            if x > max_x:
+                max_x = x
+            if x < min_x:
+                min_x = x
+            if y > max_y:
+                max_y = y
+            if y < min_y:
+                min_y = y
+    return min_x, max_x, min_y, max_y
+
 
 def part_one(filename):
     data = read_puzzle_input(filename)
+    rows_of_points = parse_points(data)
+    min_x, max_x, min_y, max_y = determine_xy_min_max(rows_of_points)
+    cm = CaveMap(min_x, max_x, min_y, max_y)
+    print(cm)
+    print(rows_of_points)
     return -1
 
 
