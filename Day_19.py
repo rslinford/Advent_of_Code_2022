c55c0de1a871f5eef5a0075674b4a677bc1ecfd3
Scott <scott@linfords.com>
2022-12-20 13:32:07 -0800

    Day 19 part one in progress

--- Day_19.py
+++ Day_19.py
@@ -1,12 +1,19 @@
 import re
 import unittest
 from dataclasses import dataclass
+from enum import Enum, auto
 
 
 def read_puzzle_input(filename):
     with open(filename, 'r') as f:
         data = f.read().strip().split('\n')
     return data
+
+class Resource(Enum):
+    ORE = auto()
+    CLAY = auto()
+    OBSIDIAN = auto()
+    GEODE = auto()
 
 
 @dataclass
@@ -19,6 +26,7 @@ class Blueprint:
     geode_robot_ore_cost: int
     geode_robot_obsidian_cost: int
 
+
 @dataclass
 class Inventory:
     ore_robot: int = 1
@@ -29,20 +37,82 @@ class Inventory:
     clay: int = 0
     obsidian: int = 0
     geode: int = 0
+    ore_robot_on_order: int = 0
+    clay_robot_on_order: int = 0
+    obsidian_robot_on_order: int = 0
+    geode_robot_on_order: int = 0
+
 
 def parse_puzzle_input(data):
     rval = []
     for line in data:
-        result = re.search('Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.', line)
+        result = re.search(
+            'Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.',
+            line)
         blueprint = Blueprint(int(result.group(1)), int(result.group(2)), int(result.group(3)), int(result.group(4)),
                               int(result.group(5)), int(result.group(6)), int(result.group(7)))
         rval.append(blueprint)
-    return
+    return rval
+
+
+def collect_ore(inventory):
+    inventory.ore += inventory.ore_robot
+    inventory.clay += inventory.clay_robot
+    inventory.obsidian += inventory.obsidian_robot
+    inventory.geode += inventory.geode_robot
+
+
+def spend_resources(blueprint, inventory):
+    if blueprint.geode_robot_ore_cost <= inventory.ore and blueprint.geode_robot_obsidian_cost <= inventory.obsidian:
+        inventory.geode_robot_on_order = 1
+        inventory.ore -= blueprint.geode_robot_ore_cost
+        inventory.obsidian -= blueprint.geode_robot_obsidian_cost
+        return
+    if blueprint.obsidian_robot_ore_cost <= inventory.ore and blueprint.obsidian_robot_clay_cost <= inventory.clay:
+        inventory.obsidian_robot_on_order = 1
+        inventory.ore -= blueprint.obsidian_robot_ore_cost
+        inventory.clay -= blueprint.obsidian_robot_clay_cost
+        return
+    if blueprint.clay_robot_ore_cost <= inventory.ore:
+        inventory.clay_robot_on_order = 1
+        inventory.ore -= blueprint.clay_robot_ore_cost
+        return
+    if blueprint.ore_robot_ore_cost <= inventory.ore:
+        inventory.ore_robot_on_order = 1
+        inventory.ore -= blueprint.ore_robot_ore_cost
+        return
+
+
+def deploy_new_robots(inventory):
+    inventory.ore_robot += inventory.ore_robot_on_order
+    inventory.ore_robot_on_order = 0
+    inventory.clay_robot += inventory.clay_robot_on_order
+    inventory.clay_robot_on_order = 0
+    inventory.obsidian_robot += inventory.obsidian_robot_on_order
+    inventory.obsidian_robot_on_order = 0
+    inventory.geode_robot += inventory.geode_robot_on_order
+    inventory.geode_robot_on_order = 0
+
+
+def evaluate_blueprint(blueprint: Blueprint, inventory: Inventory):
+    for minute in range(1, 25):
+        deploy_new_robots(inventory)
+        spend_resources(blueprint, inventory)
+        collect_ore(inventory)
+        print(f'== Minute {minute} ==')
+        print(inventory)
+
+
+def evaluate_blueprints(blueprints):
+    for bp in blueprints:
+        inventory = Inventory()
+        evaluate_blueprint(bp, inventory)
+
 
 def part_one(filename):
     data = read_puzzle_input(filename)
     blueprints = parse_puzzle_input(data)
-    inventory = Inventory()
+    total = evaluate_blueprints(blueprints)
     return -1
 
 
@@ -58,11 +128,11 @@ print(f'Answer part one: {part_one(short_filename)}')
 print(f'Answer part two: {part_two(short_filename)}')
 
 
-class Test(unittest.TestCase):
-    def test_part_one(self):
-        self.assertEqual(-1, part_one(short_filename))
-        self.assertEqual(-1, part_one(long_filename))
-
-    def test_part_two(self):
-        self.assertEqual(-1, part_two(short_filename))
-        self.assertEqual(-1, part_two(long_filename))
+# class Test(unittest.TestCase):
+#     def test_part_one(self):
+#         self.assertEqual(-1, part_one(short_filename))
+#         self.assertEqual(-1, part_one(long_filename))
+#
+#     def test_part_two(self):
+#         self.assertEqual(-1, part_two(short_filename))
+#         self.assertEqual(-1, part_two(long_filename))
