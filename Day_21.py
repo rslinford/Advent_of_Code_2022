b192e02e56f1f3294bfbdedfd5f9e1b9e4d0cb5b
Scott <scott@linfords.com>
2022-12-22 12:19:33 -0800

    Day 21 part one nearly done

--- Day_21.py
+++ Day_21.py
@@ -24,17 +24,44 @@ class Monkey:
     job: str
 
     def is_yelling(self):
-        return self.job.isnumeric()
+        try:
+            int(self.job)
+            return True
+        except ValueError:
+            return False
+
+    def parse_job(self) -> tuple[str, str, str]:
+        result = re.search('(....) (.) (....)$', self.job)
+        return result.group(1), result.group(2), result.group(3)
+
+    def solve(self, operand1, operator, operand2):
+        op1 = int(operand1)
+        op2 = int(operand2)
+        match operator:
+            case '+':
+                self.job = str(op1 + op2)
+            case '-':
+                self.job = str(op1 - op2)
+            case '*':
+                self.job = str(op1 * op2)
+            case '/':
+                self.job = str(op1 // op2)
 
 
 def part_one(filename):
     data = read_puzzle_input(filename)
     monkeys = parse_puzzle_input(data)
-    for monkey in monkeys.values():
-        if monkey.is_yelling():
-            continue
-
-    return -1
+    while True:
+        for monkey in monkeys.values():
+            if monkey.is_yelling():
+                continue
+            operand1, operator, operand2 = monkey.parse_job()
+            if not monkeys[operand1].is_yelling() or not monkeys[operand2].is_yelling():
+                continue
+            monkey.solve(monkeys[operand1].job, operator, monkeys[operand2].job)
+            if monkey.id == 'root':
+                return int(monkey.job)
+    # raise ValueError('root not found')
 
 
 def part_two(filename):
@@ -49,11 +76,11 @@ print(f'Answer part one: {part_one(short_filename)}')
 print(f'Answer part two: {part_two(short_filename)}')
 
 
-# class Test(unittest.TestCase):
-#     def test_part_one(self):
-#         self.assertEqual(-1, part_one(short_filename))
-#         self.assertEqual(-1, part_one(long_filename))
-#
-#     def test_part_two(self):
-#         self.assertEqual(-1, part_two(short_filename))
-#         self.assertEqual(-1, part_two(long_filename))
+class Test(unittest.TestCase):
+    def test_part_one(self):
+        self.assertEqual(152, part_one(short_filename))
+        self.assertEqual(-1, part_one(long_filename))
+
+    def test_part_two(self):
+        self.assertEqual(-1, part_two(short_filename))
+        self.assertEqual(-1, part_two(long_filename))
