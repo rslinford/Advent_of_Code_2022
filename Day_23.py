f1248699a391858d32e02d0b5fc86f4bf01af8dc
Scott <scott@linfords.com>
2022-12-26 15:44:07 -0800

    Day 23 initial commit

---
+++ Day_23.py
@@ -0,0 +1,35 @@
+import re
+import unittest
+
+
+def read_puzzle_input(filename):
+    with open(filename, 'r') as f:
+        data = f.read().strip().split('\n')
+    return data
+
+
+def part_one(filename):
+    data = read_puzzle_input(filename)
+    return -1
+
+
+def part_two(filename):
+    data = read_puzzle_input(filename)
+    return -1
+
+
+day_of_month = '23'
+long_filename = f'Day_{day_of_month}_long_input.txt'
+short_filename = f'Day_{day_of_month}_short_input.txt'
+print(f'Answer part one: {part_one(short_filename)}')
+print(f'Answer part two: {part_two(short_filename)}')
+
+
+class Test(unittest.TestCase):
+    def test_part_one(self):
+        self.assertEqual(-1, part_one(short_filename))
+        self.assertEqual(-1, part_one(long_filename))
+
+    def test_part_two(self):
+        self.assertEqual(-1, part_two(short_filename))
+        self.assertEqual(-1, part_two(long_filename))
