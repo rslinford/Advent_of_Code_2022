f6e9bdd7786489d98a12057fac38227daf67a776
Scott <scott@linfords.com>
2024-02-06 15:40:16 -0800

    Day 17 stubbed

---
+++ Day_17.py
@@ -0,0 +1,36 @@
+import unittest
+from collections import deque
+
+from colorama import Fore
+
+
+def read_puzzle_input(filename):
+    with open(filename, 'r') as f:
+        data = f.read().strip()
+    return data
+
+
+def parse_data(data: str):
+    return list(data)
+
+
+def part_one(filename):
+    data = read_puzzle_input(filename)
+    gas_jets = parse_data(data)
+    return -1
+
+
+def part_two(filename):
+    data = read_puzzle_input(filename)
+    gas_jets = parse_data(data)
+    return -1
+
+
+class Test(unittest.TestCase):
+    def test_part_one(self):
+        self.assertEqual(-1, part_one('Day_17_long_input.txt'))
+        self.assertEqual(-1, part_one('Day_17_short_input.txt'))
+
+    def test_part_two(self):
+        self.assertEqual(-1, part_two('Day_17_long_input.txt'))
+        self.assertEqual(-1, part_two('Day_17_short_input.txt'))
