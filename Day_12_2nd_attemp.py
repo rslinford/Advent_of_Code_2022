8bb6da9b141a3d1c25d22f97f932206048fe1c9d
Scott <scott@linfords.com>
2024-02-06 10:35:47 -0800

    Day 12 part one passes

--- Day_12_2nd_attemp.py
+++ Day_12_2nd_attemp.py
@@ -1,4 +1,7 @@
 import unittest
+from collections import deque
+
+from colorama import Fore
 
 
 def read_puzzle_input(filename):
@@ -7,9 +10,105 @@ def read_puzzle_input(filename):
     return data
 
 
+def parse_data(data: str):
+    return [list(a) for a in data.splitlines()]
+
+
+class Graph:
+    def __init__(self, grid):
+        self.grid = grid
+
+    def find_start(self):
+        for y, row in enumerate(self.grid):
+            for x, col in enumerate(row):
+                if col == 'S':
+                    return x, y
+        assert False
+
+    def find_end(self):
+        for y, row in enumerate(self.grid):
+            for x, col in enumerate(row):
+                if col == 'E':
+                    return x, y
+        assert False
+
+    def in_bounds(self, location):
+        x, y = location
+        height = len(self.grid)
+        width = len(self.grid[0])
+        return (0 <= x < width) and (0 <= y < height)
+
+    def elevation_at(self, location):
+        x, y = location
+        match self.grid[y][x]:
+            case 'S':
+                c = 'a'
+            case 'E':
+                c = 'z'
+            case _:
+                c = self.grid[y][x]
+        return ord(c) - ord('a')
+
+    def print_grid(self, x_curr, y_curr, visited):
+        for y, row in enumerate(self.grid):
+            for x, col in enumerate(row):
+                if x == x_curr and y == y_curr:
+                    print(Fore.RED + col, end='')
+                elif (x, y) in visited:
+                    print(Fore.BLUE + col, end='')
+                else:
+                    print(Fore.LIGHTWHITE_EX + col, end='')
+            print()
+
+    def neighbors(self, location):
+        x, y = location
+        target_locations = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+        target_locations = list(filter(self.in_bounds, target_locations))
+        rval = []
+        for target_location in target_locations:
+            climb = self.elevation_at(target_location) - self.elevation_at(location)
+            if climb <= 1:
+                rval.append(target_location)
+        return rval
+
+
+def bfs(graph):
+    start = graph.find_start()
+    end = graph.find_end()
+    frontier = deque()
+    frontier.append(start)
+    came_from = dict()
+    came_from[start] = None
+    while frontier:
+        current = frontier.popleft()
+        if current == end:
+            break
+        for next_loc in graph.neighbors(current):
+            if next_loc not in came_from:
+                frontier.append(next_loc)
+                came_from[next_loc] = current
+    return came_from
+
+
+def reconstruct_path(graph, came_from):
+    start = graph.find_start()
+    end = graph.find_end()
+    current = end
+    path = []
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    path.reverse()
+    return path
+
+
 def part_one(filename):
     data = read_puzzle_input(filename)
-    return -1
+    grid = parse_data(data)
+    graph = Graph(grid)
+    came_from = bfs(graph)
+    path = reconstruct_path(graph, came_from)
+    return len(path)
 
 
 def part_two(filename):
@@ -19,8 +118,8 @@ def part_two(filename):
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(-1, part_one('Day_12_input.txt'))
-        self.assertEqual(-1, part_one('Day_12_short_input.txt'))
+        self.assertEqual(425, part_one('Day_12_input.txt'))
+        self.assertEqual(31, part_one('Day_12_short_input.txt'))
 
     def test_part_two(self):
         self.assertEqual(-1, part_two('Day_12_input.txt'))
---
+++ Day_12_scratch.py
@@ -0,0 +1,92 @@
+import numpy as np
+import networkx as nx
+from time import time
+
+
+def timer_func(func):
+    # This function shows the execution time of
+    # the function object passed
+    def wrap_func(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f'Function {func.__name__!r} executed in {(t2 - t1):.4f}s')
+        return result
+
+    return wrap_func
+
+
+def up_val(ix, iy, alt_map):
+    if ord(alt_map[ix - 1, iy]) - ord(alt_map[ix, iy]) > 1:
+        return 0
+    else:
+        return 1
+
+
+def down_val(ix, iy, alt_map):
+    if ord(alt_map[ix + 1, iy]) - ord(alt_map[ix, iy]) > 1:
+        return 0
+    else:
+        return 1
+
+
+def left_val(ix, iy, alt_map):
+    if ord(alt_map[ix, iy - 1]) - ord(alt_map[ix, iy]) > 1:
+        return 0
+    else:
+        return 1
+
+
+def right_val(ix, iy, alt_map):
+    if ord(alt_map[ix, iy + 1]) - ord(alt_map[ix, iy]) > 1:
+        return 0
+    else:
+        return 1
+
+
+@timer_func
+def day12(filepath, if_any=False):
+    with open(filepath) as fin:
+        alt_map = np.genfromtxt(filepath, delimiter=1, dtype=str)
+
+    start = tuple(np.argwhere(alt_map == 'S')[0])
+    end = tuple(np.argwhere(alt_map == 'E')[0])
+    alt_map[start] = 'a'
+    alt_map[end] = 'z'
+
+    G = nx.DiGraph()
+
+    for ix, iy in np.ndindex(alt_map.shape):
+        if ix > 0:
+            if up_val(ix, iy, alt_map):
+                G.add_edge((ix, iy), (ix - 1, iy))
+        if ix < alt_map.shape[0] - 1:
+            if down_val(ix, iy, alt_map):
+                G.add_edge((ix, iy), (ix + 1, iy))
+        if iy > 0:
+            if left_val(ix, iy, alt_map):
+                G.add_edge((ix, iy), (ix, iy - 1))
+        if iy < alt_map.shape[1] - 1:
+            if right_val(ix, iy, alt_map):
+                G.add_edge((ix, iy), (ix, iy + 1))
+
+    if not if_any:
+        steps = nx.dijkstra_path_length(G, start, end)
+    else:
+        starts = [tuple(x) for x in np.argwhere(alt_map == 'a')]
+        results = nx.multi_source_dijkstra(G, starts, end)
+        steps = results[0]
+
+    return steps
+
+
+def main():
+    # assert day12('test12') == 31
+    print(day12('Day_12_short_input.txt'))
+
+    # assert day12('test12', True) == 29
+    print(day12('Day_12_short_input.txt', True))
+
+
+if __name__ == '__main__':
+    main()
